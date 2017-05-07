173777917e59e74526768b72cb8c3451a40daa7a
Jack Olivieri <boldorider4@gmail.com>
2017-05-07 13:35:53 +0000

    [Lesson 10] Add exercise .14-.15

--- L10-transfer_learning/01_lab/train_feature_extraction.py
+++ L10-transfer_learning/01_lab/train_feature_extraction.py
@@ -1,4 +1,5 @@
 import pickle
+import os.path
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
@@ -12,11 +13,9 @@ with open('train.p', mode='rb') as f:
 nb_classes = len(np.unique(dataset['labels']))
 
 # Split data into training and validation sets
-dataset_size = dataset['features'].shape[0]
-train_size = int(dataset_size*.9)
-train_features, train_labels = dataset['features'][:train_size], dataset['labels'][:train_size]
+train_features, valid_features, train_labels, valid_labels = \
+    train_test_split(dataset['features'], dataset['labels'], test_size=0.33, random_state=0)
 trainset_size = train_features.shape[0]
-valid_features, valid_labels = dataset['features'][train_size:], dataset['labels'][train_size:]
 validset_size = valid_features.shape[0]
 
 # Define placeholders and resize operation.
@@ -34,7 +33,7 @@ fc7 = tf.stop_gradient(fc7)
 
 # Add the final layer for traffic sign classification.
 shape = (fc7.get_shape().as_list()[-1], nb_classes)  # use this shape for the weight matrix
-# fc8, 1000
+# fc8
 fc8W = tf.Variable(tf.truncated_normal(shape, stddev=.1))
 fc8b = tf.Variable(tf.zeros(nb_classes))
 logits = tf.nn.xw_plus_b(fc7, fc8W, fc8b)
@@ -47,44 +46,62 @@ loss_operation = tf.reduce_mean(cross_entropy)
 # Optimizer (Adam Optimizer)
 learning_rate = .01
 optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate)
-training_operation = optimizer.minimize(loss_operation)
+training_operation = optimizer.minimize(loss_operation, var_list=[fc8W, fc8b])
 
 # Accuracy
 correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
 accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 # Train and evaluate the feature extraction model.
-epochs = 5
-batch_size = 512
+epochs = 10
+batch_size = 128
+divider = 1
+actual_trainset_size = int(trainset_size/(divider*batch_size))*batch_size
+actual_validset_size = int(validset_size/(divider*batch_size))*batch_size
 
-def evaluate(X_data, y_data, num_examples):
+saver = tf.train.Saver()
+
+def evaluate(X_data, y_data, num_examples, tfsess):
     total_accuracy = 0
-    sess = tf.get_default_session()
+    total_loss = 0
     for offset in range(0, num_examples, batch_size):
         batch_x, batch_y = X_data[offset:offset+batch_size], y_data[offset:offset+batch_size]
-        accuracy, loss = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y})
+        accuracy, loss = tfsess.run((accuracy_operation, loss_operation), feed_dict={x: batch_x, y: batch_y})
         total_accuracy += (accuracy * len(batch_x))
-    return total_accuracy / num_examples
+        total_loss += (loss * len(batch_x))
+    return (total_accuracy/num_examples, total_loss/num_examples)
 
 with tf.Session() as sess:
 
     sess.run(tf.global_variables_initializer())
-    print("Initializing model")
+    if os.path.isfile('./alexnet_optim.meta'):
+        saver.restore(sess, "./alexnet_optim")
+        print()
+        print("Model restored")
+    else:
+        print()
+        print("Initializing model")
+
     print("Training...")
     print()
 
     for i in range(epochs):
         train_features, train_labels = shuffle(train_features, train_labels)
-        for offset in range(0, trainset_size, batch_size):
+        for offset in range(0, actual_trainset_size, batch_size):
             end = offset + batch_size
-            batch_x, batch_y = train_features[offset:end], train_labels[offset:end]
-            sess.run(training_operation, feed_dict={x: batch_x, y: batch_y})
+            train_batch_x, train_batch_y = train_features[offset:end], train_labels[offset:end]
+            sess.run(training_operation, feed_dict={x: train_batch_x, y: train_batch_y})
             
-        training_accuracy, training_loss = evaluate(train_features, train_labels, trainset_size)
-        validation_accuracy, validation_loss = evaluate(valid_features, train_labels, validset_size)
+        training_accuracy, training_loss = evaluate(train_features, train_labels, actual_trainset_size, sess)
+        validation_accuracy, validation_loss = evaluate(valid_features, valid_labels, actual_validset_size, sess)
 
         print("Epoch {} ...".format(i+1))
+        print("Training Loss = {:.3f}".format(training_loss))
+        print("Validation Loss = {:.3f}".format(validation_loss))
         print("Training Accuracy = {:.3f}".format(training_accuracy))
         print("Validation Accuracy = {:.3f}".format(validation_accuracy))
         print()
+
+    saver.save(sess, './alexnet_optim')
+    print("Model saved")
  
