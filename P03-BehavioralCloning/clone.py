48de741050557d30fe16c0204374510622fdc572
Jack Olivieri <boldorider4@gmail.com>
2017-05-14 12:05:35 +0200

    [Project 03] Enhance behavioral model

--- P03-BehavioralCloning/clone.py
+++ P03-BehavioralCloning/clone.py
@@ -3,7 +3,8 @@ import cv2
 import numpy as np
 
 lines = []
-with open('./my_data/driving_log.csv') as csvfile:
+train_dir_name = 'my_data'
+with open('./' + train_dir_name + '/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         lines.append(line)
@@ -13,7 +14,7 @@ measurements = []
 for line in lines[1:]:
     source_path = line[0]
     filename = source_path.split('/')[-1]
-    current_path = './my_data/IMG/' + filename
+    current_path = './' + train_dir_name + '/IMG/' + filename
     image = cv2.imread(current_path)
     images.append(image)
     measurement = float(line[3])
@@ -23,12 +24,18 @@ X_train = np.array(images)
 y_train = np.array(measurements)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers.core import Flatten, Dense, Dropout, Lambda
+from keras.layers.convolutional import Conv2D
+from keras.layers.pooling import AveragePooling2D
 
 model = Sequential()
-model.add(Flatten(input_shape=X_train.shape[1:]))
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=X_train.shape[1:]))
+model.add(Conv2D(nb_filter=80, nb_row=10, nb_col=10, activation='relu'))
+model.add(Dropout(.7))
+model.add(Flatten())
+model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(optimizer='adam', loss='mse')
-model.fit(X_train, y_train, nb_epoch=7, validation_split=.2, shuffle=True)
+model.fit(X_train, y_train, nb_epoch=4, validation_split=.2, shuffle=True)
 model.save('model.h5')
