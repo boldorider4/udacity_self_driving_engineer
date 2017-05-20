928f07bcb3a55bb7d0ef8073f77a942bd1f127af
Jack Olivieri <boldorider4@gmail.com>
2017-05-20 22:22:18 +0200

    [Project 03] Add more comments to model script

--- P03-BehavioralCloning/clone.py
+++ P03-BehavioralCloning/clone.py
@@ -5,11 +5,13 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import os.path
 
+# generator function to yield batches of samples used to train or validate a model
 def generator(samples, batch_size=32):
     num_samples = len(samples)
     while 1: # Loop forever so the generator never terminates
         sklearn.utils.shuffle(samples)
         for offset in range(0, num_samples, batch_size):
+            # create batch
             batch_samples = samples[offset:offset+batch_size]
 
             images = []
@@ -26,6 +28,7 @@ def generator(samples, batch_size=32):
                 img_center = mpimg.imread(current_path + batch_sample[0].split('/')[-1].lstrip())
                 img_left = mpimg.imread(current_path + batch_sample[1].split('/')[-1].lstrip())
                 img_right = mpimg.imread(current_path + batch_sample[2].split('/')[-1].lstrip())
+                # mirror images horizontally
                 img_center_flipped = np.fliplr(img_center)
                 img_left_flipped = np.fliplr(img_left)
                 img_right_flipped = np.fliplr(img_right)
@@ -46,20 +49,26 @@ def generator(samples, batch_size=32):
             # trim image to only see section with road
             X_train = np.array(images)
             y_train = np.array(angles)
+            #yield batch
             yield sklearn.utils.shuffle(X_train, y_train)
 
 lines = []
+# name of directory containing driving_log.csv and IMG directory 
 train_dir_name = 'my_data'
+# open driving log used to get image filenames and corresponding steering values
 with open('./' + train_dir_name + '/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         lines.append(line)
 
+# split dataset into training and validation set
 from sklearn.model_selection import train_test_split
 train_samples, validation_samples = train_test_split(lines[1:], test_size=.2)
 
-nb_train_samples = len(lines) * 3 * 2 # number of lines x 3 (one for each center/left/right file) x 2 (flipped images)
-nb_validation_samples = nb_train_samples
+# number of lines x 3 (one for each center/left/right file) x 2 (flipped images)
+nb_train_samples = len(lines) * 3 * 2
+nb_validation_samples = nb_train_samples * .2
+# create batch generators for training and validation sets
 train_generator = generator(train_samples, batch_size=64)
 validation_generator = generator(validation_samples, batch_size=64)
 
@@ -94,11 +103,14 @@ model.compile(optimizer='adam', loss='mse')
 if os.path.isfile('./model_weights.h5'):
     print('loaded weights into model!')
     model.load_weights('model_weights.h5', by_name=True)
+# if no model weight is present, try and load entire model
 elif os.path.isfile('./model.h5'):
     print('loaded entire model!')
     model = load_model('model.h5')
 
+# change learning rate when fine tune
 model.optimizer.lr.assign(0.01)
+# run training session
 history_object = model.fit_generator(train_generator, samples_per_epoch=nb_train_samples, \
                                      validation_data=validation_generator, \
                                      nb_val_samples=nb_validation_samples, nb_epoch=5)
