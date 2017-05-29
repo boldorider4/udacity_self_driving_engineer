36d90015b29f85b910823c1c55d4e5324289fdd2
Jack Olivieri <boldorider4@gmail.com>
2017-05-29 23:20:47 +0200

    [Project 04] Add lecture .17

---
+++ P04-AdvancedLaneFinding/Lecture/03_undist_transform.py
@@ -0,0 +1,50 @@
+import pickle
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+# Read in the saved camera matrix and distortion coefficients
+# These are the arrays you calculated using cv2.calibrateCamera()
+dist_pickle = pickle.load( open( "wide_dist_pickle.p", "rb" ) )
+mtx = dist_pickle["mtx"]
+dist = dist_pickle["dist"]
+
+# Read in an image
+img = cv2.imread('test_image2.png')
+nx = 8 # the number of inside corners in x
+ny = 6 # the number of inside corners in y
+
+def corners_unwarp(img, nx, ny, mtx, dist):
+    # Pass in your image into this function
+    # Write code to do the following steps
+    # 1) Undistort using mtx and dist
+    undist = cv2.undistort(img, mtx, dist, None, mtx)
+    # 2) Convert to grayscale
+    gray = cv2.cvtColor(undist, cv2.COLOR_BGR2GRAY)
+    # 3) Find the chessboard corners
+    ret, chesscorn = cv2.findChessboardCorners(gray, (nx, ny), None)
+    # 4) If corners found:
+    if ret:
+        # a) draw corners
+        chesscorn_drawn = cv2.drawChessboardCorners(undist, (nx, ny), chesscorn, ret)
+        # b) define 4 source points
+        src = np.float32([chesscorn[0], chesscorn[nx-1], \
+                          chesscorn[-nx], chesscorn[-1]])
+        # c) define 4 destination points
+        dst = np.float32([[35,35],[432,35],[35,312],[432,312]])*2.6
+        # d) use cv2.getPerspectiveTransform() to get M, the transform matrix
+        M = cv2.getPerspectiveTransform(src, dst)
+        # e) use cv2.warpPerspective() to warp your image to a top-down view
+        img_size = (gray.shape[1], gray.shape[0])
+        warped = cv2.warpPerspective(undist, M, img_size, flags=cv2.INTER_LINEAR)
+    return warped, M
+
+top_down, perspective_M = corners_unwarp(img, nx, ny, mtx, dist)
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+f.tight_layout()
+ax1.imshow(img)
+ax1.set_title('Original Image', fontsize=50)
+ax2.imshow(top_down)
+ax2.set_title('Undistorted and Warped Image', fontsize=50)
+plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
